5582cc12b01270cc06176bb21f91dbb8f74bc4be
Matheus <stvgamer74@gmail.com>
2023-02-22 14:27:40 -0300

    feat: :sparkles: Add id attributes to contact form inputs and add onSubmit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ const loadAge = () => {
   document.querySelector('#age').innerHTML = (`<b>Age:</b> ${new Date().getFullYear() - 2003}`);
 }
 
+function onSubmit(token) {
+  const name = document.querySelector("#form-contact-name").value;
+  const email = document.querySelector("#form-contact-email").value;
+  const subject = document.querySelector("#form-contact-subject").value;
+  const message = document.querySelector("#form-contact-message").value;
+
+  const formattedBody = `Name: ${name}\nEmail: ${email}\n\n${message}\n\n`;
+
+  window.open(`mailto:profissional.ymatheus@gmail.com?subject=${subject}&body=${encodeURIComponent(formattedBody)}`);
+}
+
 window.onload = () => {
   loadAge();
 }
