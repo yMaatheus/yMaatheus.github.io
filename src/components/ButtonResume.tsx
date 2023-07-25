befeb117f7abc96d85069a34bdcf54e9e8324e48
Matheus <ymatheus.h@gmail.com>
2023-07-25 13:28:35 -0300

    feat: :sparkles: create component button resume

---
+++ src/components/ButtonResume.tsx
@@ -0,0 +1,13 @@
+export function ButtonResume() {
+  return (
+    <section>
+      <a
+        // href={link}
+        download={true}
+        className="text-green-600 border-2 rounded border-green-600 px-6 py-4 hover:border-green-700 hover:text-white hover:bg-green-700"
+      >
+        Student Resume
+      </a>
+    </section>
+  );
+}
