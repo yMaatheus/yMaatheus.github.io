d6453ab3db40f0745a914e3bc503f508cd375ddd
Matheus <ymatheus.h@gmail.com>
2023-07-28 17:00:04 -0300

    feat: :sparkles: implements open my resume on click button resume

--- src/components/ButtonResume.tsx
+++ src/components/ButtonResume.tsx
@@ -2,8 +2,8 @@ export function ButtonResume() {
   return (
     <section>
       <a
-        href=""
-        download
+        href="/curriculo-matheus-henrique.pdf"
+        target="_blank"
         className="text-green-600 border-2 rounded border-green-600 px-6 py-4 hover:border-green-700 hover:text-white hover:bg-green-700"
       >
         Currículo
